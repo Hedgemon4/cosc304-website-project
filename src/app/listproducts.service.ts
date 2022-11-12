a93962d67d5c76cd4556161f19ccb5a1e5da1e19
Hedgemon4 <sethlinc4@gmail.com>
2022-11-11 17:05:48 -0800

    Got items from backend

--- src/app/listproducts.service.ts
+++ src/app/listproducts.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient, HttpHeaders } from "@angular/common/http";
 import {ProductSearch} from "./product-search";
+import {Product} from "./product";
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,12 @@ export class ListproductsService {
   constructor(private http: HttpClient) {
   }
 
-  getProducts(productName: string): Observable<Object> {
+  products : Product[] = []
+
+  getProducts(productName: string): Observable<any> {
     let ps = new class implements ProductSearch {
       product = productName;
     }
-    return this.http.post<ProductSearch>('http://127.0.0.1/listprod', ps, { headers: new HttpHeaders().set('Content-Type', 'application/json')})
+    return this.http.post('http://127.0.0.1/listprod', ps, { headers: new HttpHeaders().set('Content-Type', 'application/json')})
   }
 }
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {ListproductsService} from "../listproducts.service";
+import {Product} from "../product";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-shop',
@@ -11,17 +13,25 @@ export class ShopComponent implements OnInit {
 
   productName : string ="";
 
+  products : Product[] = [];
+
   constructor(private http: HttpClient, private productService: ListproductsService) { }
 
   ngOnInit(): void {
+    this.sendSearch()
   }
 
   sendSearch() {
-    this.productService.getProducts(this.productName).subscribe()
+    // this.productService.getProducts(this.productName).subscribe(data => {
+    //   console.log(data)
+    // })
+    this.productService.getProducts(this.productName).subscribe((data:Product[]) => this.products = data)
+    console.log(this.products)
   }
 
   resetSearch() {
     this.productName = "";
+    this.sendSearch()
   }
 
 }
